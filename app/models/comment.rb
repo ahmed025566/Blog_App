a1a5a9baf006f342ecafdcaa2d4022894ac0601b
ahmed025566 <ahmedsalah025566@gmail.com>
2023-08-30 23:55:48 +0300

    Insert data and add some methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :post
+
+  def increment_comments_count
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :user
   belongs_to :post
+  def increment_likes_count
+    post.increment!(:likes_counter)
+  end
 end
