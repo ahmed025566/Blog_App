0c234f14e4d5ec1c010d3239b0923accea1cf747
ahmed025566 <ahmedsalah025566@gmail.com>
2023-08-31 11:21:20 +0300

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :post
 
- 
-
   def increment_comments_count
     post.increment!(:comments_counter)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,6 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: user_id
   belongs_to :post
 
- 
-
   def increment_likes_count
     post.increment!(:likes_counter)
   end
