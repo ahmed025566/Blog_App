ead51068020d7cd7df4d33b118475affd253d205
ahmed025566 <ahmedsalah025566@gmail.com>
2023-09-04 21:07:52 +0300

    Add views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
   resources :users, only: [:index, :show]
   get '/users/:id/posts', to: 'posts#index', as: 'user_posts'
   get '/users/:id/posts/:id', to: 'posts#show', as: 'user_post'
