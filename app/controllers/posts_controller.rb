3d52dca4b5e9b8f1ab14dc2df9f477f333cfbc7f
ahmed025566 <ahmedsalah025566@gmail.com>
2023-09-07 23:31:22 +0300

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,26 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @author = User.find(params[:user_id])
+    @post = @author.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_path(id: @post.author_id)
+    else
+      render :new, alert: 'An error has occurred while creating the post'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
