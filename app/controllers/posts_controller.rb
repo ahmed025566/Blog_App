7c3845c4eb0db7e629c3dc4e7e3733c1318043f0
ahmed025566 <ahmedsalah025566@gmail.com>
2023-09-07 17:57:33 +0300

    Add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 end
