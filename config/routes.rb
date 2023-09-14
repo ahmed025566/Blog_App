93b6f9688e148ee459e788bc4efe789cd521334a
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-09-14 15:20:03 -0400

    create route for API

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:new, :create]
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show, :new, :create, :destroy] do
+        resources :comments, only: [:new, :create, :destroy]
+        resources :likes, only: [:new, :create]
+      end
     end
   end
 end
