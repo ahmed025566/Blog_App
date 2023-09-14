e56b4c163198d2711ede208aa2f34ac7d1b2a25c
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-09-14 15:38:34 -0400

    add content for post and comment controllers

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,7 @@
 class Api::PostsController < ApplicationController
-end
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+      render json: @posts
+    end
+  end
