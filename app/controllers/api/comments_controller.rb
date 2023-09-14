ae355b3fec49b13cd52a34c3d5e96343bdfd33ff
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-09-14 15:41:27 -0400

    autofix rubocop

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,25 +1,25 @@
 class Api::CommentsController < ApplicationController
-    def index
-      @post = Post.find(params[:post_id])
-      @comments = @post.comments
-      render json: @comments
-    end
-  
-    def create
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.build(comment_params)
-      @comment.user = current_user
-      
-      if @comment.save
-        render json: @comment, status: :created
-      else
-        render json: @comment.errors, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text)
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    render json: @comments
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
